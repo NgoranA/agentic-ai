38a752c56a794dc4cbb141b738709061577b6cb8
NgoranA <ngoranaristide4@gmail.com>
2025-07-19 10:36:35 +0100

    added the possibility to run scripts.

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -5,23 +5,33 @@ import subprocess
 def run_python_file(working_directory, file_path, args=[]):
     absolut_working_director = os.path.abspath(working_directory)
     target_file = os.path.abspath(os.path.join(working_directory, file_path))
+
     if not target_file.startswith(absolut_working_director):
-        return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+
     if not os.path.exists(target_file):
         return f'Error: File "{file_path}" not found.'
+
     if not target_file.endswith(".py"):
         return f'Error: "{file_path}" is not a Python file.'
 
     try:
         complete_child_process = subprocess.run(
-            args,
-            executable=target_file,
+            ["python3", target_file] + args,
             capture_output=True,
             timeout=30,
             cwd=absolut_working_director,
         )
-        if not complete_child_process:
+        output = []
+        output.append(
+            f"STDOUT:{complete_child_process.stdout} STDERR:{complete_child_process.stderr}"
+        )
+        if complete_child_process.returncode != 0:
+            output.append(
+                f"Process exited with code {complete_child_process.returncode}"
+            )
+        if not output:
             return "No output produced"
-        return f"STDOUT: {complete_child_process.stdout} STDERR: {complete_child_process.stderr}"
+        return output
     except Exception as e:
         return f"Error: executing Python file: {e}"
---
+++ previoustests.py
@@ -0,0 +1,41 @@
+
+    # result = get_files_info("calculator", ".")
+    # print("Result for current directory:")
+    # print(result)
+    # print("")
+    #
+    # result = get_files_info("calculator", "pkg")
+    # print("Result for 'pkg' directory:")
+    # print(result)
+    #
+    # result = get_files_info("calculator", "/bin")
+    # print("Result for '/bin' directory:")
+    # print(result)
+    #
+    # result = get_files_info("calculator", "../")
+    # print("Result for '../' directory:")
+    # print(result)
+    #
+    #
+    #     result = get_file_content("calculator", "main.py")
+    # print(result)
+    #
+    # result = get_file_content("calculator", "pkg/calculator.py")
+    # print(result)
+    #
+    # result = get_files_info("calculator", "/bin/cat")
+    # print(result)
+    #
+    # result = get_file_content("calculator", "pkg/does_not_exist.py")
+    # print(result)
+    #
+
+        # result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")            
+        # print(result)                                                                             
+        # result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")      
+        # print(result)                                                                             
+        # result = write_file("calculator", "pkg/ddyy/morelorem.txt", "lorem ipsum dolor sit amet") 
+        # print(result)                                                                             
+        # result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")          
+        # print(result)                                                                             
+                                                                                                    
